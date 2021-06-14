8c2162328f48cb8fc5dc4292438b2a7d45283efc
Rashpreet Kaur <69358864+rashpreet97@users.noreply.github.com>
2021-06-14 10:44:42 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,20 @@
+<?php if(!isset($_SESSION)){
+	session_start();
+	}  
+?>
+
+<?php include('header.php'); ?>
+	<div class="dashboard" style="background-color:#fff;">
+		<h3 class="text-middle" style="background-color:#272327;color: #fff;padding: 5px;">Admin Panel</h3>
+		<span class="adminDashboard" style="font-size: 85px;font-weight: bold;color: blue;font-family: serif;margin-left: 180px;background-color: black;">Welcome To Admin Panel</span>
+	</div>
+	
+	
+<?php include('footer.php'); ?>
+
+</div><!--  containerFluid Ends -->
+
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
--- header.php
+++ header.php
@@ -11,10 +11,20 @@
 	</style>
 </head>
 <body>
-	<div class="container-fluid">
-		<div class="header_top">
-			<!-- <a href="index.php"><img src="img/dash2.jpg" class="img-responsive" alt="logo.png img"></a> -->
 
+
+<?php
+		if($_SESSION['adminstatus'] == ""){
+			header("location:adminlogin.php");
+		}
+		
+		   
+
+	 ?>
+
+
+<div class="container-fluid">
+		<div class="header_top">
 			<span style="font-size:50px;color:#2c2f84;font-weight:bolder;margin-left:15px;">Doctor Consultation System</span>
 		</div>
 
@@ -22,12 +32,15 @@
 	<div class="navbar navbar-default nav">
 		<nav class="menu">
 			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="doctorinfo.php">Doctors</a></li>
-				<li><a href="about.php">About Us</a></li>
-				<li><a href="contactus.php">Contact Us</a></li>
-				<li><a href="signin.php">Login</a></li>
 				
+				
+				
+				<li><a href="addDoctor2.php">Add Doctor</a></li>
+				<li><a href="viewDoctor.php">View Doctor</a></li>
+				<li><a href="viewPatient.php">View Patient</a></li>
+				<li><a href="viewAppointment.php">View Appointment</a></li>
+				<li><a href="viewFeedback.php">View Feedback</a></li>
+				<li><a href="logout.php">Logout</a></li>
 			</ul>
 		</nav>
 	</div>
